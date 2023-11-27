1a2ece464f78d8cb2ecc542a101dabcb07ae0d17
Arafat Sani <yasirarafat9287@gmail.com>
2023-11-27 07:04:47 -0500

    first commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema ({
+    email: {
+        type: String,
+        //required: true,
+        unique: true
+    }
+});
+
+const options = {
+    IncorrectPasswordError: 'Password or username are incorrect',
+    IncorrectUsernameError: 'Password or username are incorrect',
+};
+
+UserSchema.plugin(passportLocalMongoose, options);
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ seeds/index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {
+    places,
+    descriptors
+} = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+});
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: "6535123c3254b5b7a0d81eba",
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].latitude,
+                    cities[random1000].longitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dwxykvqno/image/upload/v1699205744/YelpCamp/pmkl7jnkrptblummu6pg.jpg',
+                    filename: 'YelpCamp/pmkl7jnkrptblummu6pg'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwxykvqno/image/upload/v1699205744/YelpCamp/poxpxhvlrcq3gxbullnx.jpg',
+                    filename: 'YelpCamp/poxpxhvlrcq3gxbullnx'
+                }
+            ]
+        })
+        await camp.save();
+    }
+
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
+}
